cb64b21782eeb38e8882af7f368038061d3a81d5
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-21 11:43:05 -0500

    adding jquery to main.handlebars, removing map reference in controllers, adding my first route template

--- controllers/school_controllers.js
+++ controllers/school_controllers.js
@@ -2,15 +2,14 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
-var maps = require("./maps");
+
 
 // temporary route
 router.get("/", function(req, res) {
     res.render("index");
 });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 //currently this prints the schools where the zips match. 
 //so i need a way to display this data in the index-2 template. 
 // could maybe even do a join here with the alderman table to include alderman information
@@ -30,19 +29,15 @@ router.get("/api/:zip", function(req,res){
 
 
 
-
-
-=======
-=======
 router.get("/MAP", function(req,res) {
     // initMap();
     res.render("mapExp");
 
 })
 
->>>>>>> master
+
 router.get("/2", function(req, res) {
     res.render("index-2");
 });
->>>>>>> master
+
 module.exports = router;
