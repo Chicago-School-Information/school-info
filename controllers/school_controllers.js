19a18b871f000ed94c985bce733f698f131a112f
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-21 13:00:23 -0500

    got rid of the reference to the map testing page in the router.

--- controllers/school_controllers.js
+++ controllers/school_controllers.js
@@ -2,10 +2,6 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 
 // temporary route
 router.get("/", function(req, res) {
@@ -28,14 +24,6 @@ router.get("/api/:zip", function(req,res){
         res.json(schools);
         console.log(res.json(schools));
     })
-})
-
-
-
-router.get("/MAP", function(req,res) {
-    // initMap();
-    res.render("mapExp");
-
 })
 
 
