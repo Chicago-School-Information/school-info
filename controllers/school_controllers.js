75fe4664c409a55e939525f98318efd1f5ebe74a
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-21 11:59:04 -0500

    Moved map stuff to index 2

--- controllers/school_controllers.js
+++ controllers/school_controllers.js
@@ -2,7 +2,6 @@
 var express = require("express");
 var router = express.Router();
 var db = require("../models");
-var maps = require("./maps");
 
 // temporary route
 router.get("/", function(req, res) {
