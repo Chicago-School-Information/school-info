2899fe60e8c6797a00abc346578cf8370148d258
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-22 14:31:36 -0500

    adding separate frontend js file for school zip serch, made the call get instead of post, and updated names in main.handlebars

--- public/js/schools.js
+++ public/js/schools.js
@@ -1,70 +1,21 @@
-var address = "";
-
-// function sendZip(zip) {
-//     $.post("/api/", zip, function () {
-//         console.log("i got hit");
-//     });
-// }
-//         $("#submit").on("click", function (event) {
-//             event.preventDefault();
-//             let userZip = $("#zipcode").val().trim();
-//             sendZip(userZip);
-//             })
-         
-        
-        // $(".petition-button").on("click", function (event) {
-        //     alert("submit button hit")
-        // })
+$(document).ready(function () {
 
 
-    var map;
-    var infowindow;
-    function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: {lat: 41.8781, lng: -87.6298}
-    });
-    infowindow = new google.maps.InfoWindow();
+    function sendZip(zip) {
+        console.log("first part worked");
+        $.get("/api/" + zip,
+            {
+                zip: zip,
+            },
+            function (data, status) {
+                console.log(data);
+            });
 
-    map.data.loadGeoJson('assets/map_data/Chicago_Public_Schools-School_Locations_SY1415.geojson');
-    map.data.addListener('click', function(event){
-        var feat = event.feature;
-        var html = "<b>"+feat.getProperty('school_nm')+"</b><br>"+feat.getProperty('sch_addr');
-        infowindow.setContent(html);
-        infowindow.setPosition(event.latLng);
-        infowindow.setOptions({pixelOffset: new google.maps.Size(0,-34)});
-        infowindow.open(map);
+    }
+    $("#submit").on("click", function (event) {
+        event.preventDefault();
+        let userZip = $("#zipcode").val().trim();
+        console.log(userZip);
+        sendZip(userZip);
     })
-
-    var geocoder = new google.maps.Geocoder();
-
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeAddress(geocoder, map);
-    });
-    }
-
-    function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('zipcode').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-        resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-            map: resultsMap,
-            position: results[0].geometry.location,
-            icon: {
-                path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                scale: 5,
-                fillColor: 'blue',
-                fillOpacity: 1,
-                strokeColor: 'blue',
-                strokeWeight: 5
-            }
-        });
-        } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-    }
-
-
-
+})
