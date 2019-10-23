87fa6666ca5fa7afef8b77e2c17e78e99d2268f9
woaky1 <53833473+woaky1@users.noreply.github.com>
2019-10-22 21:23:28 -0500

    fixed it so the sidebar is cleared when you do another search so you only see the results of your latest search. Added more rows of info and did a bit of formatting to make it easier to read.

--- public/js/schools.js
+++ public/js/schools.js
@@ -10,10 +10,14 @@ $(document).ready(function () {
         },
             function (data, status) {
                 console.log(data);
-                          
+                $(".school-report-cards").empty();
                 for (var i = 0; i < data.schools.length; i++) {
                     var school_name = data.schools[i].school_name;
-                    $(".school-report-cards").append(school_name);
+                    var school_level = data.schools[i].level;
+                    var school_safety = data.schools[i].safety_icon;
+                    var instruction_score = data.schools[i].instruction_score;
+                    $(".school-report-cards").append(`<ul><li>Name: ${school_name}</li><li>School Level: ${school_level}</li><li>Safety Strength: ${school_safety}</li><li>Instruction Score: ${instruction_score}</li></ul><hr>`);
+
                 };
                 
             })
