2683b3f31fcb72f44a8655aaa032ee76ba237b91
GG Guitart <gertaguitart2019@u.northwestern.edu>
2019-10-22 21:32:20 -0500

    got the alderman name to show up

--- public/js/schools.js
+++ public/js/schools.js
@@ -3,19 +3,21 @@ $(document).ready(function () {
 
     function sendZip(zip) {
         console.log("first part worked");
-
+        console.log(data)
         $.get("/api/" + zip,
-        {
-            zip: zip
-        },
+            {
+                zip: zip
+            },
             function (data, status) {
                 console.log(data);
-                          
+
                 for (var i = 0; i < data.schools.length; i++) {
                     var school_name = data.schools[i].school_name;
                     $(".school-report-cards").append(school_name);
+                    $("<br>");
+                    $(".school-report-cards").append(data.schools[i].Alderman.alderman);
                 };
-                
+
             })
     }
 
